00e8a86c3aeff35075e06330915c98b662471594
gbussier <gbussier@Inception.myguest.virtualbox.org>
2024-03-20 15:51:43 +0100

    push inception

--- srcs/requirements/mariadb/tool/script.sh
+++ srcs/requirements/mariadb/tool/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-service mysql stqrt;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+service mariadb  start;
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON '\`${MYSQL_DATABASE}\`'.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+exec mariadbd-safe
