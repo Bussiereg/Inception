1cbf27b7c7d2bc6ca6cef53e0ec85cc0d9bc7009
gbussier <gbussier@InceptionDeb.myguest.virtualbox.org>
2024-04-04 18:12:13 +0200

    maj

--- srcs/requirements/wordpress/tool/auto-config.sh
+++ srcs/requirements/wordpress/tool/auto-config.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-set -a
-source .env
-set +a
-
 sleep 10
+
+directory="/run/php"
+
+# Check if the directory exists
+if [ ! -d "$directory" ]; then
+    # Create the directory
+    sudo mkdir -p "$directory"
+    echo "Directory $directory created successfully."
+else
+    echo "Directory $directory already exists."
+fi
 
 wp config create --allow-root \
             --dbname=$MYSQL_DATABASE \
             --dbuser=$MYSQL_USER \
             --dbpass=$MYSQL_PASSWORD \
-            --dbhost=mariadb:3306 --path='/var/www/wordpress'
+            --dbhost=mariadb:3306 \
+            --path='/var/www/wordpress/'
 
 
-wp core install --allow-root \ 
+wp core install --allow-root \
             --url=$DOMAIN_NAME \
             --title=$WP_TITLE \
             --admin_user=$WP_ADMIN_USER \
@@ -21,6 +29,9 @@ wp core install --allow-root \
             --admin_email=$WP_ADMIN_EMAIL \
             --skip-email
 
-wp user create --allow-root \ 
+wp user create --allow-root \
             --user-login=$WP_USER2 \
             --user_pass=$WP_USER2_PASSWORD \
+    
+
+/usr/sbin/php-fpm7.4 -F
