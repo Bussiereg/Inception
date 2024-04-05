22fa2e17d57d5b8fa23b2897dc996866d4828b5b
gbussier <gbussier@Inception.myguest.virtualbox.org>
2024-04-05 17:51:10 +0200

    push inception

--- srcs/requirements/mariadb/tool/script.sh
+++ srcs/requirements/mariadb/tool/script.sh
@@ -5,17 +5,16 @@ source .env
 set +a
 
 service mariadb  start;
-echo "----test1----"
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-echo "----test2----"
+sleep 2
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-echo "----test3----"
+sleep 2
 mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-echo "----test4----"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-echo "----test5----"
+
+# mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+sleep 2
 mariadb -e "FLUSH PRIVILEGES;"
-echo "----test6----"
+sleep 2
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-echo "----test7----"
+sleep 2
 exec mariadbd-safe
