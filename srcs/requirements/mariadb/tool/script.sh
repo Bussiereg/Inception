885e6fbaefbd63014f4c65b66dc0674f43f9db32
gbussier <gbussier@Inception.myguest.virtualbox.org>
2024-04-16 18:01:37 +0200

    push inception

--- srcs/requirements/mariadb/tool/script.sh
+++ srcs/requirements/mariadb/tool/script.sh
@@ -12,10 +12,10 @@ echo "grant privileges"
 mariadb -u root -p${MYSQL_ROOT_PASSWORD}  -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 echo "add password for root"
 mariadb -u root -p${MYSQL_ROOT_PASSWORD}  -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-sleep 2
+sleep 1
 echo "reset privileges"
 mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
-sleep 2
+sleep 1
 echo "restart mysql"
 mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 sleep 2
