4f73f99f603ef6f528e40d49c00aaffe17715d05
gbussier <gbussier@Inception.myguest.virtualbox.org>
2024-04-19 14:39:21 +0200

    push inception

--- srcs/requirements/wordpress/tool/auto-config.sh
+++ srcs/requirements/wordpress/tool/auto-config.sh
@@ -5,7 +5,7 @@ sleep 20
 cd var/
 
 wp config create --allow-root \
-                --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER \
+                --dbname=$MYSQL_DATABASE --dbuser=$(<"/run/secrets/db_user") \
                 --dbpass=$(<"/run/secrets/db_password") --dbhost=mariadb:3306 \
                 --path='/var/www/wordpress'
 
@@ -13,7 +13,7 @@ echo "wp core install......."
 wp core install --allow-root \
             --url=$DOMAIN_NAME \
             --title='inception' \
-            --admin_user=$WP_ADMIN_USER \
+            --admin_user=$(<"/run/secrets/wp_admin_user") \
             --admin_password=$(<"/run/secrets/wp_admin_password") \
             --admin_email='gadmin.g@gmail.com' \
             --skip-email \
@@ -21,7 +21,7 @@ wp core install --allow-root \
 
 echo "wp user create........"
 wp user create --allow-root \
-            $WP_USER2 guser2.g@gmail.com --user_pass=$(<"/run/secrets/wp_user2_password") \
+            $(<"/run/secrets/wp_user") guser2.g@gmail.com --user_pass=$(<"/run/secrets/wp_user_password") \
             --path='/var/www/wordpress'
 
 directory="/run/php"
